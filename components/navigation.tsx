049a876c115508b13f15d90092b96e73d123bebf
Hanzla Haroon <62152476+hanzlaharoon@users.noreply.github.com>
2025-01-02 22:53:22 +0500

    fix(nav): added styling in navbar

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -2,17 +2,17 @@ import Link from "next/link";
 
 export default function Navigation() {
   return (
-    <nav className="sticky h-14 inset-x-0 top-0 z-30 w-full bg-white/75 backdrop-blur-lg transition-all">
+    <nav className="sticky h-20 inset-x-0 top-0 z-30 w-full mb-4 bg-secondary backdrop-blur-lg transition-all">
       {/* <div className="mx-auto w-full max-w-lg px-2.5"> */}
       <div className="container md:mx-auto px-4 w-full">
-        <div className="flex h-14 items-center space-x-2">
-          <Link href="/" className="h3 flex z-40 font-semibold">
-            <span>File Management</span>
+        <div className="flex h-20 items-center space-x-5">
+          <Link href="/" className="h3 flex z-40 font-semibold text-2xl mr-4">
+            <span>File Management App</span>
           </Link>
-          <Link href="/fileslist" className="flex z-40 font-semibold">
+          <Link href="/fileslist" className="flex z-40  text-lg">
             <span>Files</span>
           </Link>
-          <Link href="/newfile" className="flex z-40 font-semibold">
+          <Link href="/newfile" className="flex z-40  text-lg">
             <span>New File</span>
           </Link>
         </div>
